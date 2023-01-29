6549b6d6433410d9827bd6fc91d64eb38396564b
Hadjati Darius <darius.hadjati@gmail.com>
2023-01-29 15:01:43 +0100

    the break keyword

--- Section 19 - Repeating Stuff With Loops/While_Loops/app.js
+++ Section 19 - Repeating Stuff With Loops/While_Loops/app.js
@@ -1,7 +1,19 @@
-const magicWord = 'Please';
+/* const magicWord = 'Please';
 
 let guess = prompt("Say the magic word");
 while (guess !== magicWord){
     guess = prompt("Say the magic word");
 }
-console.log("CONGRATS!");
+console.log("CONGRATS!"); */
+
+
+
+
+let say = prompt("Hey, say something!")
+while (true){
+    say = prompt(say);
+    if (say.toLowerCase() === "stop copying me") {
+        break;
+        }
+}
+console.log('OK, YOU WIN!');
