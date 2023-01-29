c01d4dae63c2268a5ae6356e35d304577d0fa6f3
Hadjati Darius <darius.hadjati@gmail.com>
2023-01-29 15:54:45 +0100

    guessing game

--- Section 19 - Repeating Stuff With Loops/While_Loops/app.js
+++ Section 19 - Repeating Stuff With Loops/While_Loops/app.js
@@ -9,11 +9,39 @@ console.log("CONGRATS!"); */
 
 
 
-let say = prompt("Hey, say something!")
+/* let say = prompt("Hey, say something!")
 while (true){
     say = prompt(say);
     if (say.toLowerCase() === "stop copying me") {
         break;
         }
 }
-console.log('OK, YOU WIN!');
+console.log('OK, YOU WIN!'); */
+
+
+let maximum = parseInt(prompt("Please enter a maximum number"));
+while (!maximum) {
+    maximum = parseInt(prompt("Enter a valid number!"));
+}
+
+const targetNumber = Math.floor(Math.random() * maximum) + 1;
+
+let guess = parseInt(prompt("Enter guess"));
+let attempts = 1;
+
+while (parseInt(guess) !== targetNumber) {
+    if (guess === 'q') break;
+    attempts++;
+    if (guess < targetNumber) {
+        guess = prompt("Too low! Enter guess");
+    } else {
+        guess = prompt("Too high! Enter guess");
+    }
+}
+if (guess === 'q') {
+    console.log('QUIT');
+} else {
+    console.log('CONGRATS YOU WIN!');
+    console.log(`It took you ${attempts} guesses`);
+
+}
