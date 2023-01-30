86649c03db6a67ff3958a8117e6ec134c1ada0a9
Hadjati Darius <darius.hadjati@gmail.com>
2023-01-30 18:43:39 +0100

    solving practice exercises for functions

--- Section 20 - Introducing Functions/Basics/app.js
+++ Section 20 - Introducing Functions/Basics/app.js
@@ -41,3 +41,91 @@ return num1*num2;
 console.log(multiply(5,4));
 
 
+
+//It should accept a single number argument, which we will call temperature
+//If temperature is greater than or equal to 75, return true. 
+//Otherwise, return false.   
+
+function isShortsWeather(temperature){
+if (temperature >= 75) {
+    return true;
+}else {
+    return false;
+}
+}
+
+console.log(isShortsWeather(100));
+
+
+
+/* Please write a function called lastElement which accepts a single array 
+argument.  The function should return the last element of the 
+array (without removing the element).  
+If the array is empty, the function should return null. */
+
+function lastElement(array) {
+    if (array.length === 0) {
+        return null;
+    } else {
+        return array[array.length-1];
+    }
+}
+
+//Define a function called capitalize that accepts a string 
+//argument and returns a new string with the first letter capitalized 
+
+function capitalize(str) {
+    let capitalStr = str[0].toUpperCase() + str.slice(1);
+    return capitalStr;
+}
+console.log(capitalize("lol"));
+
+
+
+// Write a function called sumArray which accepts a single argument: an array of numbers. 
+//  It should return the sum of all the numbers in the array.
+function sumArray(numbers) {
+    let total = 0;
+    for (const number of numbers) {
+        total += number;
+    }
+    return total;
+}
+
+/* Write a function called returnDay. 
+this function takes in one parameter (a number from 1-7) 
+and returns the day of the week (1 is Monday, 2 is Tuesday, etc.)
+  If the number is less than 1 or greater than 7, 
+  the function should return null. 
+  In some countries Sunday is treated as the first day of the week, 
+  but for this exercise we will use Monday as the first day. */
+
+  function returnDay(number) {
+    switch (number) {
+        case 1:
+            return 'Monday';
+            break;
+        case 2:
+            return 'Tuesday';
+            break;    
+        case 3:
+            return 'Wednesday';
+            break;    
+        case 4:
+            return 'Thursday';
+            break;    
+        case 5:
+            return 'Friday';
+            break;    
+        case 6:
+            return 'Saturday';
+            break;   
+        case 7:
+            return 'Sunday';
+            break;    
+        default:
+            return null;
+            break;
+    }
+  }
+  console.log(returnDay(8));
