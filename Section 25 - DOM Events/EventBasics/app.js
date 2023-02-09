a5d74c1d5d4202dbc9ad0a4bb53fe65b2674e6ae
Hadjati Darius <darius.hadjati@gmail.com>
2023-02-09 13:57:44 +0100

    try out addEventlistener

--- Section 25 - DOM Events/EventBasics/app.js
+++ Section 25 - DOM Events/EventBasics/app.js
@@ -7,3 +7,14 @@ btnHungry.onclick = () => (console.log("Lets eat then!"))
 btnNotHungry.onmouseover = () => (btnNotHungry.innerText = "Yes!");
 btnNotHungry.onmouseout = () => (btnNotHungry.innerText = "No");
 btnNotHungry.onclick = () => (console.log("Lets eat then!"));
+
+const hello = document.querySelector('#hello');
+const bye = document.querySelector('#goodbye');
+
+
+hello.addEventListener('click', () => {
+    console.log('hello');
+})
+bye.addEventListener('click', () => {
+    console.log('goodbye');
+})
