dcb052f9263fad5e91e9fa142098a958c4bb0219
Hadjati Darius <darius.hadjati@gmail.com>
2023-02-11 16:20:41 +0100

    chain async functions with await keywords

--- Section 26 - Async JS/AsyncFunctions/app.js
+++ Section 26 - Async JS/AsyncFunctions/app.js
@@ -1,4 +1,4 @@
-async function login(username, password) {
+/* async function login(username, password) {
     if (!username || !password) throw "No credentials"
     if (password === "alohomora") return "Welcome"
     throw "Invalid Password"
@@ -12,4 +12,41 @@ login('user', 'alohomora')
     .catch(err => {
         console.log('ERROR');
         console.log(err);
+    }) */
+
+
+
+const delayedColorChange = (color, delay) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            document.body.style.backgroundColor = color;
+            resolve();
+        }, delay)
     })
+}
+
+/* delayedColorChange('olive', 1000)
+.then(()=> (delayedColorChange('violet', 1000)))
+.then(()=> (delayedColorChange('orange', 1000)))
+.then(()=> (delayedColorChange('pink', 1000)))
+.then(()=> (delayedColorChange('red', 1000))) */
+
+
+async function rainbow() {
+    await delayedColorChange('violet', 1000)
+    await delayedColorChange('orange', 1000)
+    await delayedColorChange('pink', 1000)
+    await delayedColorChange('green', 1000)
+    await delayedColorChange('blue', 1000)
+    await delayedColorChange('red', 1000)
+    return "ALL DONE!"
+}
+
+// rainbow().then(() => console.log('END OF RAINBOW'))
+
+async function printRainbow() {
+    await rainbow();
+    console.log('END OF RAINBOW');
+} 
+
+printRainbow();
