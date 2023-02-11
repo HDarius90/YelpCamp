efa0c7d3d862e3036f9f1edefaf72562072912d7
Hadjati Darius <darius.hadjati@gmail.com>
2023-02-11 16:36:58 +0100

    Handling errors in async functions

--- Section 26 - Async JS/AsyncFunctions/app.js
+++ Section 26 - Async JS/AsyncFunctions/app.js
@@ -32,7 +32,7 @@ const delayedColorChange = (color, delay) => {
 .then(()=> (delayedColorChange('red', 1000))) */
 
 
-async function rainbow() {
+/* async function rainbow() {
     await delayedColorChange('violet', 1000)
     await delayedColorChange('orange', 1000)
     await delayedColorChange('pink', 1000)
@@ -49,4 +49,30 @@ async function printRainbow() {
     console.log('END OF RAINBOW');
 } 
 
-printRainbow();
+printRainbow(); */
+
+
+const fakeRequestPromise = (url) => {
+    return new Promise((resolve, reject) => {
+       const delay = Math.floor(Math.random() * 4500) + 500;
+        setTimeout(() => {
+            if (delay > 2000){
+                reject('Connection Timeout :(')
+            }else {
+                resolve(`Here is your fake data from ${url}`)
+            }
+       }, delay) 
+    })
+}
+
+async function makeTwoRequests(){
+    try {
+        let data1 = await fakeRequestPromise("/page1")
+        console.log(data1);
+        let data2 = await fakeRequestPromise("/page2")
+        console.log(data2);
+    } catch (error) {
+        console.log('CAUGHT AN ERROR');
+        console.log(error);
+    }
+ }
