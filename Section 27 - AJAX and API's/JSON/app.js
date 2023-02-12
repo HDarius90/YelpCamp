d335190df3368ad0cc73b1788bcb160e1e60d5fa
Hadjati Darius <darius.hadjati@gmail.com>
2023-02-12 15:21:20 +0100

    using fetch instead of XML

--- Section 27 - AJAX and API's/JSON/app.js
+++ Section 27 - AJAX and API's/JSON/app.js
@@ -1,4 +1,4 @@
-const req = new XMLHttpRequest();
+/* const req = new XMLHttpRequest();
 req.onload = function () {
     console.log("IT LOADED!!!");
     console.log(this.responseText);
@@ -11,12 +11,28 @@ req.onerror = function () {
 }
 
 req.open("GET", "https://official-joke-api.appspot.com/random_joke");
-req.send();
+req.send(); */
+
+
+
+
+fetch('https://official-joke-api.appspot.com/random_joke')
+.then((res) => {
+    console.log("RESOLVED!", res);
+    return res.json();
+})
+.then((data) => {
+    displayJoke(data)
+})
+.catch((e)=>{
+    console.log('error', e);
+})
+
 
 
 function displayJoke(data) {
 
-    const parsedData = JSON.parse(data);
+    const parsedData = data;
 
     const h6 = document.querySelector('h6');
     const h3 = document.querySelector('h3');
