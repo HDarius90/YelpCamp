af385e4e2fde0507710ce4a1760419d197e3059b
Hadjati Darius <darius.hadjati@gmail.com>
2023-02-13 17:47:35 +0100

    using class

--- Section 28 - Async JS/Section 29 - Prototypo, classa, OOP/OOP/app.js
+++ Section 28 - Async JS/Section 29 - Prototypo, classa, OOP/OOP/app.js
@@ -1,4 +1,4 @@
-
+/* 
 function MakeMath(a, b) {
     this.a = a;
     this.b = b;
@@ -10,4 +10,43 @@ MakeMath.prototype.calculate = function () {
 }
 
 const firstMath = new MakeMath(5, 6);
-firstMath.calculate();
+firstMath.calculate(); */
+
+
+
+class MakeMath {
+    constructor(a, b) {
+        this.a = a;
+        this.b = b;
+    }
+    add(){
+        const {a, b} = this;
+        return  a + b;
+    }
+}
+
+const firstMath = new MakeMath(5,6);
+console.log(firstMath.add());
+
+
+
+
+
+class Color {
+    constructor(r, g, b, name) {
+        this.r = r;
+        this.g = g;
+        this.b = b;
+        this.name = name;
+    }
+    greet(){
+        return `HELLO FROM ${this.name}`
+    }
+    rgb(){
+        const {r,g,b} = this;
+        return `rgb(${r}, ${g}, ${b})`
+    }
+}
+
+const c1 = new Color(255, 67, 89, 'tomato');
+
