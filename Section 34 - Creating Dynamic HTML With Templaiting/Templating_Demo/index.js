6bf48289edf73ea1622aed3967ec56c3832223ca
Hadjati Darius <darius.hadjati@gmail.com>
2023-02-22 22:47:15 +0100

    Passing Data To Templates

--- Section 34 - Creating Dynamic HTML With Templaiting/Templating_Demo/index.js
+++ Section 34 - Creating Dynamic HTML With Templaiting/Templating_Demo/index.js
@@ -6,8 +6,13 @@ app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, '/views'))
 
 
-app.get('/', (req,res)=>{
+app.get('/', (req, res) => {
     res.render('home');
+})
+
+app.get('/rand', (req, res) => {
+    const num = Math.floor(Math.random() * 10) + 1;
+    res.render('random', { rand: num })
 })
 
 app.listen(3000, () => {
