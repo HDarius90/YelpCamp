0f15fb42dc85ebb0857fa11dfdcc6e6e20619419
Hadjati Darius <darius.hadjati@gmail.com>
2023-02-26 14:42:42 +0100

    RESTful Comments Index

--- Section 35 - Defining RESTful Routes/RESTDemo/index.js
+++ Section 35 - Defining RESTful Routes/RESTDemo/index.js
@@ -1,8 +1,39 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 app.use(express.json()) // for parsing application/json
+
+const comments = [
+    {
+        author: "Sanya",
+        comment: "Csá csumi csá csokoládés csőtészta!!!!"
+    },
+    {
+        author: "Dari",
+        comment: "Força Barça"
+    },
+    {
+        author: "Sziszi",
+        comment: "Nyunyu???!"
+    },
+    {
+        author: "Nyunyu",
+        comment: "Pakoljá ki!!!"
+    }
+];
+
+app.get('/comments', (req, res) => {
+    res.render('comments/index', { comments })
+})
+
+
+
 
 
 app.get('/tachos', (req, res) => {
@@ -14,6 +45,8 @@ app.post('/tachos', (req, res) => {
     res.send(`Here is your ${qty} ${type} tacho`);
     console.log(req.body);
 });
+
+
 
 app.listen(3000, () => {
     console.log('ON PORT 3000!');
