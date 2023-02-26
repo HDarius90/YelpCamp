a5c0fecd4bd2a3620119ecb7f813f11fa277c1aa
Hadjati Darius <darius.hadjati@gmail.com>
2023-02-26 12:47:36 +0100

    Parsing The Request Body

--- Section 35 - Defining RESTful Routes/RESTDemo/index.js
+++ Section 35 - Defining RESTful Routes/RESTDemo/index.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const app = express();
 
-app.get('/tachos', (req, res)=>{
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.use(express.json()) // for parsing application/json
+
+
+app.get('/tachos', (req, res) => {
     res.send('GET /tacho request');
 });
 
-app.post('/tachos', (req, res)=>{
-    res.send('POST /tacho request');
+app.post('/tachos', (req, res) => {
+    const { type, qty } = req.body;
+    res.send(`Here is your ${qty} ${type} tacho`);
+    console.log(req.body);
 });
 
 app.listen(3000, () => {
