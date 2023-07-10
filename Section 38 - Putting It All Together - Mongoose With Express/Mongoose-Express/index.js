4a4567b8d770ff7267e39f1019add743fd89f9c9
Hadjati Darius <hadjati.darius90@gmail.com>
2023-07-10 13:18:17 +0200

    Creating Our Model

--- Section 38 - Putting It All Together - Mongoose With Express/Mongoose-Express/index.js
+++ Section 38 - Putting It All Together - Mongoose With Express/Mongoose-Express/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
+const Product = require('./models/product.js');
 
 mongoose.connect('mongodb://127.0.0.1:27017/shop')
     .then(() => {
