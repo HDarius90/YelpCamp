d5dff6a8803243738d69b39b978765f00dee66d3
Darius Hadjati <darius.hadjati@gmail.com>
2023-07-19 13:53:47 +0200

    Deleting Products

--- Section 38 - Putting It All Together - Mongoose With Express/Mongoose-Express/index.js
+++ Section 38 - Putting It All Together - Mongoose With Express/Mongoose-Express/index.js
@@ -56,6 +56,12 @@ app.put('/products/:id', async (req, res) => {
     res.redirect(`/products/${id}`)
 })
 
+app.delete('/products/:id', async (req, res) => {
+    const { id } = req.params;
+    const deletedProduct = await Product.findByIdAndDelete(id);
+    res.redirect('/products')
+})
+
 
 
 
