981ad3c0aa157a8903e9006492cb1c3afccf98a9
Darius Hadjati <darius.hadjati@gmail.com>
2023-07-20 21:28:45 +0200

    Using Morgan - Logger Middleware

---
+++ Section 40 - Middleware - The Key To Express/Middleware_Intro/index.js
@@ -0,0 +1,17 @@
+const  express = require('express');
+const app = express();
+const morgan = require('morgan')
+
+app.use(morgan('tiny'))
+
+app.get('/', (req, res) =>{
+    res.send('HOME PAGE')
+})
+
+app.get('/dogs', (req, res) =>{
+    res.send('WOOF WOOF!')
+})
+
+app.listen(3000, () => {
+    console.log("Listening on port 3000");
+})
