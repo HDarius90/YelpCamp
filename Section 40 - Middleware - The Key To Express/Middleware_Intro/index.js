eef50abc10404ce022621219ef30b9047fd24ef3
Darius Hadjati <darius.hadjati@gmail.com>
2023-07-21 18:08:19 +0200

    Protecting Specific Routes

--- Section 40 - Middleware - The Key To Express/Middleware_Intro/index.js
+++ Section 40 - Middleware - The Key To Express/Middleware_Intro/index.js
@@ -9,13 +9,13 @@ app.use((req, res, next) => {
     next();
 })
 
-app.use((req, res, next) => {
+const verifyPassword = (req, res, next) => {
     const { password } = req.query;
     if (password === 'alahomora') {
         next()
     }
     res.send("SORRY, YOU NEED A PASSWORD")
-})
+}
 
 app.use('/dogs', (req, res, next) => {
     console.log('DOG');
@@ -45,7 +45,7 @@ app.get('/dogs', (req, res) => {
     res.send('WOOF WOOF!')
 })
 
-app.get('/secret', (req, res) =>{
+app.get('/secret', verifyPassword, (req, res) => {
     res.send('MY SECRET IS: Sometimes I war headphones in public so I Dont have to talk to peaople')
 })
 
