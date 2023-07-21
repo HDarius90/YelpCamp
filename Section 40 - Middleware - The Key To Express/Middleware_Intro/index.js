a2f808dcf9fa83c86bd7cbefd08ca00e4b0fdda8
Darius Hadjati <darius.hadjati@gmail.com>
2023-07-21 17:42:08 +0200

    Setting Up A 404 Route

--- Section 40 - Middleware - The Key To Express/Middleware_Intro/index.js
+++ Section 40 - Middleware - The Key To Express/Middleware_Intro/index.js
@@ -9,6 +9,11 @@ app.use((req, res, next) =>{
     next();
 })
 
+app.use('/dogs', (req, res, next) => {
+    console.log('DOG');
+    next()
+})
+
 /* app.use((req, res, next) => {
     console.log("THIS IS MAY FIRST MIDLEWARE!");
     return next();
@@ -30,6 +35,10 @@ app.get('/', (req, res) =>{
 
 app.get('/dogs', (req, res) =>{
     res.send('WOOF WOOF!')
+})
+
+app.use((req, res) => {
+    res.status(404).send('NOT FOUND!')
 })
 
 app.listen(3000, () => {
