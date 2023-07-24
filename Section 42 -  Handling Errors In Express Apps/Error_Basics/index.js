3786c80ecea05680824e8da611dbf7dedb093baf
Darius Hadjati <darius.hadjati@gmail.com>
2023-07-24 20:22:46 +0200

    Our Custom Error Class

--- Section 42 -  Handling Errors In Express Apps/Error_Basics/index.js
+++ Section 42 -  Handling Errors In Express Apps/Error_Basics/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan')
 
+const AppError = require('./AppError');
+
 app.use(morgan('tiny'))
 app.use((req, res, next) => {
     req.requestTime = new Date();
@@ -14,8 +16,9 @@ const verifyPassword = (req, res, next) => {
     if (password === 'alahomora') {
         next()
     }
+    throw new AppError("Password require", 401);
     // res.send("SORRY, YOU NEED A PASSWORD")
-    throw new Error("Password require")
+    // res.status(401);
 }
 
 app.get('error', (req, res) => {
@@ -54,10 +57,20 @@ app.get('/secret', verifyPassword, (req, res) => {
     res.send('MY SECRET IS: Sometimes I war headphones in public so I Dont have to talk to peaople')
 })
 
+app.get('/admin', (req, res) => {
+    throw new AppError('You are not an admin', 403)
+})
+
 app.use((req, res) => {
     res.status(404).send('NOT FOUND!')
 })
 
+app.use((err, req, res, next) => {
+    const { status = 500, message = "Something Went Wrong" } = err;           //we can give default value to error status in case it is not an AppError and it doesnt have status property
+    res.status(status).send(message);
+})
+
+/* 
 app.use((err, req, res, next) => {
     console.log("*******************");
     console.log("***********ERROR********");
@@ -65,7 +78,7 @@ app.use((err, req, res, next) => {
     // res.status(500).send("ERROR")
     console.log(err);
     next(err);
-})
+}) */
 
 app.listen(3000, () => {
     console.log("Listening on port 3000");
