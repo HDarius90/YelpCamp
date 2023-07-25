84ba888e2e70effa3ff6b33d3123032fe4b8a349
Darius Hadjati <darius.hadjati@gmail.com>
2023-07-25 16:27:37 +0200

    JOI Schema Validations

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -4,8 +4,9 @@ const mongoose = require('mongoose');
 const Campground = require('./models/campground')
 const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
+const Joi = require('joi');
 const catchAsync = require('./utils/catchAsync');
-const ExpressError = require('./utils/ExpressError')
+const ExpressError = require('./utils/ExpressError');
 
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
 
@@ -38,7 +39,19 @@ app.get('/campgrounds/new', (req, res) => {
 });
 
 app.post('/campgrounds', catchAsync(async (req, res, next) => {
-    if(!req.body.campground) throw new ExpressError('Invalid Campground Data', 400)
+    // if(!req.body.campground) throw new ExpressError('Invalid Campground Data', 400)
+    const campgroundSchema = Joi.object({
+        campground: Joi.object({
+            title: Joi.string().required(),
+            price: Joi.number().required().min(0),
+        }).required()
+    })
+    const { error } = campgroundSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',');
+        throw new ExpressError(msg, 400);
+    }
+
     const campground = new Campground(req.body.campground)
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`)
