76f8f146ecd9802e6285ff88975a0d998da8bb1f
Darius Hadjati <darius.hadjati@gmail.com>
2023-07-25 15:00:20 +0200

    More Errors

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -4,7 +4,8 @@ const mongoose = require('mongoose');
 const Campground = require('./models/campground')
 const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
-const catchAsync = require('./utils/catchAsync')
+const catchAsync = require('./utils/catchAsync');
+const ExpressError = require('./utils/ExpressError')
 
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
 
@@ -37,12 +38,10 @@ app.get('/campgrounds/new', (req, res) => {
 });
 
 app.post('/campgrounds', catchAsync(async (req, res, next) => {
-
+    if(!req.body.campground) throw new ExpressError('Invalid Campground Data', 400)
     const campground = new Campground(req.body.campground)
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`)
-
-
 }))
 
 app.get('/campgrounds/:id', catchAsync(async (req, res) => {
@@ -67,8 +66,13 @@ app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
     res.redirect('/campgrounds')
 }))
 
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page not found!', 404))
+})
+
 app.use((err, req, res, next) => {
-    res.send("Oh boy something went wrong!")
+    const { statusCode = 500, message =  "Something went wrong!"} = err;
+    res.status(statusCode).send(message);
 })
 
 
