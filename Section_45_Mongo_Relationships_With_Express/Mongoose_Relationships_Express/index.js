f7efea81cfc98636d7c82bfbf3a93da2793b3cda
Darius Hadjati <darius.hadjati@gmail.com>
2023-07-27 18:05:54 +0200

    Creating Products For A Farm

--- Section_45_Mongo_Relationships_With_Express/Mongoose_Relationships_Express/index.js
+++ Section_45_Mongo_Relationships_With_Express/Mongoose_Relationships_Express/index.js
@@ -43,6 +43,23 @@ app.post('/farms', async (req, res) => {
 
 })
 
+app.get('/farms/:id/products/new', (req, res) => {
+    const { id } = req.params;
+    res.render('products/new', { categories, id })
+})
+
+app.post('/farms/:id/products', async (req, res) => {
+    const { id } = req.params;
+    const farm = await Farm.findById(id);
+    const { name, price, category } = req.body;
+    const product = new Product({ name, price, category })
+    farm.products.push(product);
+    product.farm = farm;
+    await farm.save();
+    await product.save();
+    res.send(farm);
+})
+
 //PRODUCTS ROUTE
 
 const categories = ['fruit', 'vegetable', 'dairy'];
