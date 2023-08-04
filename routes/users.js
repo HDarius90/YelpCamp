f123eb8e7a4781755b04f5e2ac774e212e708fa1
Hadjati Darius <hadjati.darius90@gmail.com>
2023-08-04 14:27:32 +0200

    Login Routes

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
-const catchAsync = require('../utils/catchAsync')
+const catchAsync = require('../utils/catchAsync');
+const passport = require('passport');
 
 router.get('/register', (req, res) => {
     res.render('users/register')
@@ -20,5 +21,14 @@ router.post('/register', catchAsync(async (req, res) => {
     }
 
 }))
+
+router.get('/login', (req, res) => {
+    res.render('users/login');
+})
+
+router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
+    req.flash('success', 'Welcome back');
+    res.redirect('/campgrounds')
+})
 
 module.exports = router;
