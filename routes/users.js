cd9fe98b101819cbd3ae53abc5d300269044af20
Darius Hadjati <darius.hadjati@gmail.com>
2023-08-06 21:12:08 +0200

    A Fancy Way To Restructure Routes

--- routes/users.js
+++ routes/users.js
@@ -1,25 +1,21 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/user');
 const catchAsync = require('../utils/catchAsync');
 const passport = require('passport');
 const { storeReturnTo } = require('../middleware');
 const users = require('../controllers/users');
 
+router.route('/register')
+    .get(users.renderRegister)
+    .post(catchAsync(users.register));
 
-router.get('/register', users.renderRegister);
 
-router.post('/register', catchAsync(users.register))
+router.route('/login')
+    .get(users.renderLogin)
+    .post(storeReturnTo, // use the storeReturnTo middleware to save the returnTo value from session to res.locals
+        passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }),// passport.authenticate logs the user in and clears req.session
+        users.login);        // Now we can use res.locals.returnTo to redirect the user after login
 
-router.get('/login', users.renderLogin)
-
-router.post('/login',
-    // use the storeReturnTo middleware to save the returnTo value from session to res.locals
-    storeReturnTo,
-    // passport.authenticate logs the user in and clears req.session
-    passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }),
-    // Now we can use res.locals.returnTo to redirect the user after login
-    users.login);
 
 router.get('/logout', users.logout);
 
