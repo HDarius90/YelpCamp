5a7dfb8bd4423572aeea194fec843c6ad1a477ad
Hadjati Darius <darius.hadjati@gmail.com>
2023-08-07 15:26:41 +0200

    Environment Variables with dotenv

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+if(process.env.NODE_ENV !== "production") {
+    require('dotenv').config();
+}
+
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
@@ -8,7 +12,8 @@ const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
-const User = require('./models/user')
+const User = require('./models/user');
+
 
 
 const userRoutes = require('./routes/users')
