bd4ce697c27c0799766540e8e854070a94ea4996
Hadjati Darius <darius.hadjati@gmail.com>
2023-08-07 13:44:54 +0200

    Displaying Star Ratings

--- seeds/index.js
+++ seeds/index.js
@@ -19,7 +19,7 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
-            author: '64cf4d74a6c88428c53f18d8',
+            author: '64d0d5d545241923cf887317',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: 'https://source.unsplash.com/collection/483251',
