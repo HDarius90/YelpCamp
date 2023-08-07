58e88bd22769351d11f127b4662563814e57bc69
Hadjati Darius <darius.hadjati@gmail.com>
2023-08-07 16:45:54 +0200

    Setup Cloudinary and Storing Uploaded Image Links In Mongo

--- cloudinary/index.js
+++ cloudinary/index.js
@@ -2,18 +2,20 @@ const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUDE_NAME,
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_KEY,
     api_secret: process.env.CLOUDINARY_SECRET
-})
+});
 
 const storage = new CloudinaryStorage({
     cloudinary,
-    folder: 'YelpCamp',
-    allowedFormats: ['jpeg', 'png', 'jpg']
-})
+    params: {
+        folder: 'YelpCamp',
+        allowedFormats: ['jpeg', 'png', 'jpg']
+    }
+});
 
-module.export = {
+module.exports = {
     cloudinary,
     storage
 }
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -9,10 +9,8 @@ const upload = multer({ storage });
 
 router.route('/')
     .get(catchAsync(campgrounds.index))
-    //.post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
-    .post(upload.single('image'), (req, res) => {
-        res.send(req.body)
-    })
+    .post(isLoggedIn, upload.array('image'),  validateCampground, catchAsync(campgrounds.createCampground));
+
 
 router.get('/new', isLoggedIn, campgrounds.renderNewForm);
 
